03ad6be58934e957d2196e06181368999243925d
yeonju501 <loling970124@gmail.com>
2021-08-03 08:54:19 +0900

    solved 1 problem

--- D2/1954.py
+++ D2/1954.py
@@ -1,5 +1,22 @@
-T = int(input())
-for tc in range(1, T+1):
+for T in range(int(input())):
     N = int(input())
-    snail = [[0]*N for s in range(N)]
-print(snail)
+    route = [[0]*N for _ in range(N)]
+    dx = [0, 1, 0, -1]
+    dy = [1, 0, -1, 0]
+    mode = 0
+    x = y = 0
+    route[x][y] = 1
+    for i in range(2, N**2+1):
+        x += dx[mode]
+        y += dy[mode]
+        route[x][y] = i
+        if 0 <= x + dx[mode] < N and 0 <= y + dy[mode] < N and not route[x+dx[mode]][y+dy[mode]]:
+            continue
+        if mode != 3:
+            mode += 1
+        else:
+            mode = 0
+    print(f'{T+1}')
+    for i in route:
+        print(*i)
+
