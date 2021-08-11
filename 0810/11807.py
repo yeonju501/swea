5bb0f03cc1db2a4a93094850c21ef639648fef62
yeonju501 <loling970124@gmail.com>
2021-08-11 14:10:00 +0900

    0811

--- 0810/11807.py
+++ 0810/11807.py
@@ -5,19 +5,24 @@ for tc in range(1, T+1):
     print('#{}'.format(tc), end=' ')
     # K는 간격 N은 정류장 M은 충전기
     K, N, M = map(int,input().split())
+    road = [0]*(N+K)
     stop = list(map(int, input().split()))
     result = 0
-        # for i in range(len(stop)-1):
-        #     if stop[i+1] - stop[i] > 3:
-        #         break
+    spot = 0
     for i in range(len(stop)):
-        for j in range(K, 0, -1):
-            if stop[i]+j in stop:
+        road[stop[i]] += 1
+    while 0 <= spot <= N+K:
+        for i in range(K, 0, -1):
+            if road[spot+i] == 1 and N-spot > K:
                 result += 1
-                print(stop[i])
-                N -= K
+                spot += i
                 break
-        if N <= 0:
+        else:
             break
 
+    for i in range(len(stop)-1):
+        if stop[i+1] - stop[i] > K:
+            result = 0
+            break
+    print(result)
 
